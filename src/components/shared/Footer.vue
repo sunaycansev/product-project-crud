e4844e13233a167526a1e9191f11d731363b5ade
sunaycansev <fsunaycansev@gmail.com>
2021-12-21 23:50:30 +0300

    refactor footer

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -3,20 +3,20 @@
     class="navbar fixed-bottom navbar-light"
     style="background-color: #e3f2fd"
   >
-    <span class="navbar-text mr-auto">
+    <span class="navbar-text ms-3">
       <strong>sunay</strong>
     </span>
-    <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
+    <span class="navbar-text navbar-nav my-2 my-lg-0 me-3 nav-item">
       <strong>Alış Tutarı : </strong>
-      <span class="badge badge-danger">34,56</span>
+      <span class="badge bg-danger">34,56</span>
     </span>
-    <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
+    <span class="navbar-text navbar-nav my-2 my-lg-0 me-3 nav-item">
       <strong>Satış Tutarı : </strong>
-      <span class="badge badge-success">34,56</span>
+      <span class="badge bg-success">34,56</span>
     </span>
-    <span class="navbar-text navbar-nav my-2 my-lg-0">
+    <span class="navbar-text navbar-nav my-2 my-lg-0 nav-item me-3">
       <strong>Bakiye : </strong>
-      <span class="badge badge-primary">34,56</span>
+      <span class="badge bg-primary">34,56</span>
     </span>
   </nav>
 </template>
